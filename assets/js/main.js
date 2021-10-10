b1cfb3da37451872aecb2df0ca8b1d1ac7f477e0
Etienne Schmitz <contact@etienne-schmitz.com>
2021-10-10 17:47:00 +0200

    feat - add compute score function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,81 @@
 import * as params from '@params';
 
-document
-    .getElementById("quiz")
-    .addEventListener("submit", (event) => {
-        event.preventDefault();
+const form = document.getElementById("quiz");
 
-        const doc = window.jspdf.jsPDF('landscape');
-        doc.addImage("/images/logo.png", 'JPEG', 15, 10, 16, 16)
-        doc.setFontSize(24)
-        doc.text(
-            `Certificat de suivi de l\'activité "${params.name}"`,
-            50,
-            15);
+function compute_score() {
 
-        doc.setFontSize(16)
-        let text = "Nous certifions que le détenteur de certificat a suivi l'activité sus-nommé, et qu'il a obtenu le score suivant :";
+    const questions = params.questions;
+    let score = 0;
 
-        let lines = doc.splitTextToSize(text, 150);
-        doc.text(50, 35, lines);
+    const data = new FormData(form);
 
-        doc.rect(5, 5, 220, 200, 'S');
-        doc.rect(235, 5, 50, 200, 'S');
+    for (const q of questions) {
+        switch (q.type) {
+            case 'choice':
+                if (data.get(q.id) == q.answers[q.answer_index]) {
+                    score++;
+                }
 
-        doc.setFontSize(50)
+                break
+            case 'multichoice':
+                const user_answers = data.getAll(q.id);
+                console.log(user_answers)
 
-        doc.text(
-            '10/15',
-            175,
-            55);
+                let answers = []
+                q.answer_index.forEach(index => {
+                    answers.push(q.answers[index])
+                })
+                console.log(answers)
 
-        doc.save("certificat.pdf");
-        return false;
-    });
+                if (
+                    answers.length == user_answers.length &&
+                    answers.filter(arr1Item => !user_answers.includes(arr1Item)).length == 0
+                ) {
+                    score++;
+                }
+                break
+        }
+    }
+
+    return score
+}
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    // process questions and get results
+    console.log("score " + compute_score());
+
+    return 0
+
+    // process the first page
+
+    // process all pages questions
+
+    const doc = window.jspdf.jsPDF('landscape');
+    doc.addImage("/images/logo.png", 'JPEG', 15, 10, 16, 16)
+    doc.setFontSize(24)
+    doc.text(
+        `Certificat de suivi de l\'activité "${params.name}"`,
+        50,
+        15);
+
+    doc.setFontSize(16)
+    let text = "Nous certifions que le détenteur de certificat a suivi l'activité sus-nommé, et qu'il a obtenu le score suivant :";
+
+    let lines = doc.splitTextToSize(text, 150);
+    doc.text(50, 35, lines);
+
+    doc.rect(5, 5, 220, 200, 'S');
+    doc.rect(235, 5, 50, 200, 'S');
+
+    doc.setFontSize(50)
+
+    doc.text(
+        '10/15',
+        175,
+        55);
+
+    doc.save("certificat.pdf");
+    return false;
+});
